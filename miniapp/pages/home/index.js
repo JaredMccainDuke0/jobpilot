6ec92a0f0b4ec885d800bdf10c022c3c03271c81
JaredMccainDuke0 <3467660759@qq.com>
2026-08-26 13:28:55 +0800

    feat: add mini-program resume upload flow

--- miniapp/pages/home/index.js
+++ miniapp/pages/home/index.js
@@ -7,4 +7,5 @@ Page({
     request("/api/state").then((state) => this.setData({ state, loading: false })).catch((error) => this.setData({ error: error.message, loading: false }));
   },
   openMatches() { wx.navigateTo({ url: "/pages/matches/index" }); },
+  openResume() { wx.navigateTo({ url: "/pages/resume/index" }); },
 });
